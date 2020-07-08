ba587ce062de4ce068b4fea5fa7d02ae827d6274
hellopkb <zagbitz@gmail.com>
2020-07-08 12:01:17 +1000

    intial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'agldemo-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'agldemo';
+}
